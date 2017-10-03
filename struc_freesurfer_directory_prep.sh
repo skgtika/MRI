ce0bdffc7f6fab85238eda30908c75be7f454ac9
TheSharmalab <Nik@eduroam-int-dhcp-97-34-74.ucl.ac.uk>
2017-10-03 14:15:41 +0100

    updates

--- struc_freesurfer_directory_prep.sh
+++ struc_freesurfer_directory_prep.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# It is ESSENTIAL to run this before FREESURFER
+
 ROOT="/scratch/scratch/rmjlns1/shared"
-CUT="/scratch/scratch/rmjlns1/shared/cstruc_250"
+CUT="/scratch/scratch/rmjlns1/shared/cstruc_250" # Source of the nii.gz files
 
 cd /scratch/scratch/rmjlns1/shared
 
@@ -9,10 +11,9 @@ mkdir $ROOT/freesurfer_subjects/subjects
 
 for i in {1..250} ; do
 	cd $ROOT/freesurfer_subjects/subjects
-	mksubjdirs cut_${i}
-
+	mksubjdirs cut_${i} # This is a Freesurfer command that generates all of the substructure needed 
 	cp -fr $CUT/cut_${i}_mprage.nii.gz $ROOT/freesurfer_subjects/subjects/cut_${i}/mri
 	cd $ROOT/freesurfer_subjects/subjects/cut_${i}/mri
-	mri_convert -cut_${i}_mprage.nii.gz 001.mgz
+	mri_convert -cut_${i}_mprage.nii.gz 001.mgz # Is labelled 001.mgz as Freesurfer convention  
 done
  
